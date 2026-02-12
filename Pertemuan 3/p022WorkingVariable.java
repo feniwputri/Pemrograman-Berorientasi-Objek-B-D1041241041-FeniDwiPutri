6b4f8ac7de7276f6d382945ddb7c9dffa67fe094
feniwputri <fnniidwii@gmail.com>
2026-02-12 15:25:04 +0700

    Create p022WorkingVariable.java

---
+++ Pertemuan 3/p022WorkingVariable.java
@@ -0,0 +1,20 @@
+public class p022WorkingVariable {
+public static void main(String[] args) {
+    //Deklarasi dan inisialisasi variabel
+    int angka = 10;
+
+    //Menampilkan nilai awal variabel 
+    System.out.println("Nilai awal angka :" + angka); 
+
+    //Mengubah nilai variable
+    angka = 25;
+
+    //Menampilkan nilai setelah perubahan
+     System.out.println("Nilai awal angka :" + angka); 
+}
+}
+// Nama : Feni Dwi Putri
+// NIM  : D1041241041
+// Tanggal : 12 Februari 2025
+
+
