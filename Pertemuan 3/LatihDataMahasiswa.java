cbb0dde88af107af3281a1dd59206e4cfbd89e46
feniwputri <fnniidwii@gmail.com>
2026-02-13 08:07:25 +0700

    Latihan 1
    Menambahkan Latihan 1 Pertemuan 3

---
+++ Pertemuan 3/LatihDataMahasiswa.java
@@ -0,0 +1,34 @@
+public class LatihDataMahasiswa {
+public static void main(String[] args) {
+
+        // NIM: Gunakan tipe data string karena ada huruf.
+        String nim = "D1041241041"; 
+        
+        // Nama Lengkap (Tipe String)
+        String nama = "Feni Dwi Putri";
+        
+        // Kelas (Tipe char untuk 1 huruf)
+        char kelas = 'B';
+        
+        // Status Aktif (Tipe boolean: true/false)
+        boolean statusAktif = true;
+        
+        // IPK (Tipe double untuk desimal)
+        double ipkSemester = 3.77;
+        
+        // Total SKS (Tipe int untuk bilangan bulat)
+        int totalSks = 64;
+
+        // --- 2. MENAMPILKAN OUTPUT ---
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM          : " + nim);
+        System.out.println("Nama         : " + nama);
+        System.out.println("Kelas        : " + kelas);
+        System.out.println("Status Aktif : " + statusAktif);
+        System.out.println("IPK Semester : " + ipkSemester);
+        System.out.println("Total SKS    : " + totalSks);
+    }
+}
+// Nama : Feni Dwi Putri
+// NIM  : D1041241041
+// Tanggal : 13 Februari 2025
