47d47965b4d60bed9e993c3ee0654e2d956d29ae
feniwputri <fnniidwii@gmail.com>
2026-02-13 08:48:19 +0700

    Pertemuan 4
    Menambahkan p041NamingConvention ke Pertemuan 4

---
+++ Pertemuan 4/p041NamingConvention.java
@@ -0,0 +1,18 @@
+public class p041NamingConvention {
+
+public static final double PI = 3.14;
+public static void main(String[] args) {
+// Variabel dengan camelCase
+int studentAge = 20;
+String studentName = "Budi";
+boolean isActive = true;
+System.out.println("Nama: " + studentName);
+System.out.println("Umur: " + studentAge);
+System.out.println("Status: " + isActive);
+System.out.println("Konstanta PI: " + PI);
+}
+
+}
+// Nama : Feni Dwi Putri
+// NIM  : D1041241041
+// Tanggal : 13 Februari 2025
