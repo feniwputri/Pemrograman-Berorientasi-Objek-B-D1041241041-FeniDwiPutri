9323ed3dd968e1086b5a1e27806ac8182ab70934
feniwputri <fnniidwii@gmail.com>
2026-02-26 22:38:19 +0700

    Menambahkan Latihan 3 Pertemuan 6

---
+++ Pertemuan 7/LatihanTabelPerkalian.java
@@ -0,0 +1,18 @@
+public class LatihanTabelPerkalian {
+public static void main(String[] args) {
+        int angka = 7;
+        
+    
+        System.out.println("=== TABEL PERKALIAN " + angka + " ===");
+        
+        // Gunakan for loop untuk iterasi 1-12
+        for (int i = 1; i <= 12; i++) {
+            // Menghitung hasil perkalian
+            int hasil = angka * i;
+            
+            System.out.println(angka + " × " + i + " = " + hasil);
+        }
+    }
+}
+// Nama : Feni Dwi Putri
+// NIM  : D1041241041
