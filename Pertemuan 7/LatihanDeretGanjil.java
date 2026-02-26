9f49fb68ed91277a45993e0def7ff7eb58912b80
feniwputri <fnniidwii@gmail.com>
2026-02-26 22:23:08 +0700

    Menambahkan Latihan 1 Pertemuan 7

---
+++ Pertemuan 7/LatihanDeretGanjil.java
@@ -0,0 +1,20 @@
+public class LatihanDeretGanjil {
+    public static void main(String[] args) {
+        System.out.println("=== BILANGAN GANJIL 1-15 ===");
+        
+        int angka = 1;
+        
+        while (angka <= 15) {
+           
+            // Kita menggunakan kondisi modulus untuk mengecek ganjil
+            if (angka % 2 != 0) {
+
+                System.out.print(angka + " ");
+            }
+            // Update angka agar loop tidak berjalan tanpa henti (infinite loop)
+            angka++;
+        }
+        
+        System.out.println();
+    }
+}
